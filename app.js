d6ca0a9e95956179e01f6269978f4eb7bf53acbf
ELuculent <ELuculent@users.noreply.github.com>
2017-04-04 12:25:10 +1000

    removed specific info

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const client = new BeamClient();
 // the required information to all of our requests after this call.
 client.use('oauth', {
     tokens: {
-        access: 'U2jf9cRIQFm1iGUb8uXv3W9G0FVjme3oVNelxl7CROZ2HA4fXoWcqUpxEQBF8nKz',
+        access: 'your_auth_token',
         expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
     },
 });
@@ -40,29 +40,14 @@ function createChatSocket(userId, channelId, endpoints, authkey) {
     // Chat connection
     const socket = new BeamSocket(endpoints).boot();
 
-    // React to our !pong command
+    // React to our commands
     socket.on('ChatMessage', data => {
         if (data.message.message[0].data.toLowerCase().startsWith('!ping')) {
             socket.call('msg', [`@${data.user_name} PONG!`]);
             console.log(`Ponged ${data.user_name}`);
-        } else if (data.message.message[0].data.toLowerCase().startsWith('!gt')) {
-            socket.call('msg', ['My gamertag is currently Sygma Salamanda. Add me if you want...']);
+        } else if (data.message.message[0].data.toLowerCase().startsWith('!command')) {
+            socket.call('msg', ['response']);
             console.log('Announced Gamertag');
-        } else if (data.message.message[0].data.toLowerCase().startsWith('!yt')) {
-            socket.call('msg', ['I post a lot of random videos on my youtube channel. Check it out if you want at https://www.youtube.com/user/luculentsea16']);
-            console.log('Announced Youtube');
-        }else if (data.message.message[0].data.toLowerCase().startsWith('!discord')) {
-            socket.call('msg', ['If you want to talk to me off stream or you just want somewhere to chill join my discord: https://discord.gg/824Btgy'])
-            console.log('Announced banterous discord message');
-        }else if (data.message.message[0].data.toLowerCase().startsWith('!wanderer')) {
-            socket.call('msg', ['A friend of mine. A... different kind of person. Challenge: figure out the secret. http://beam.pro/thewanderer117']);
-            console.log('Wanderer advertisement. Happy now?');
-        }else if (data.message.message[0].data.toLowerCase().startsWith('!bot')) {
-            socket.call('msg', ['I programmed this bot myself. If you want a bot just ask and I can make one for you. The best way is to fill out the form but you can just get in touch with me through beam :) https://goo.gl/forms/TaB4lXF2tbHhQTVp2']);
-            console.log('');
-        }else if (data.message.message[0].data.toLowerCase().startsWith('!')) {
-            socket.call('msg', ['']);
-            console.log('');
         }
     });
 
